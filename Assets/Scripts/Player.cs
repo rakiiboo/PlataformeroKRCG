395a72409d475f1d9f0c9b0f24a614d382a3eec7
rakiiboo <93097993+rakiiboo@users.noreply.github.com>
2022-09-18 21:29:37 -0600

    Movimiento Personaje

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody2D rigidbody2D;
+    private float horizontal;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal") * speed;
+        if (horizontal < 0.0f)
+        {
+            transform.localScale = new Vector2(-1.0f, 1.0f);
+        }
+        else if (horizontal > 0.0f)
+        {
+            transform.localScale = new Vector2(1.0f, 1.0f);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rigidbody2D.velocity = new Vector2(horizontal, rigidbody2D.velocity.y);
+    }
+
+}
