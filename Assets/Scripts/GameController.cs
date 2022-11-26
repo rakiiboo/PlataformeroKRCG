19e887f97db9af72ef6d60c235729f597e2a887d
rakiiboo <93097993+rakiiboo@users.noreply.github.com>
2022-11-26 14:44:57 -0600

    laboratorio 4

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -7,39 +7,72 @@ public class GameController : MonoBehaviour
 {
     public int level = 0;
 
-    public GameObject winSpot;
+    private GameObject winSpot;
 
     private BoxCollider2D winCollider;
 
-    public GameObject player;
+    private GameObject player;
 
     private BoxCollider2D playerCollider;
 
+    private SaveData saveData;
+
     public GameObject[] checkPoints;
 
-    public bool isFinal;
+    public int finalLevel;
+
+
+    private void  Awake() {
+        GameObject gameController=GameObject.Find("GameController");
+        DontDestroyOnLoad(gameController);
+        if(GameObject.FindGameObjectsWithTag(gameObject.tag).Length>1){
+            Destroy(gameObject);
+        }
+    }
 
     // Start is called before the first frame update
     void Start()
     {
+    }
+
+    private void OnEnable() {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode){
+        winSpot=GameObject.Find("Win");
         winCollider = winSpot.GetComponent<BoxCollider2D>();
+        player=GameObject.Find("Player");
         playerCollider = player.GetComponent<BoxCollider2D>();
+        if(saveData!=null){
+            Debug.Log("Lo que sea");
+            level = saveData.level;
+            player.transform.position = new Vector3(saveData.position[0],saveData.position[1],saveData.position[2]);
+            saveData=null;
+        }
+    }
+
+    private void OnDisable() {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
     }
 
     void Update()
     {
         if (winCollider.IsTouching(playerCollider))
         {
-            if (isFinal)
+            if (finalLevel == level)
             {
                 SceneManager.LoadScene("Win");
             }
             else
             {
-                SceneManager.LoadScene("Level" + (level + 1));
+                level+=1;
+                SceneManager.LoadScene("Level" + (level));
+            
             }
         }
-        foreach (var checkpoint in checkPoints)
+
+        /*foreach (var checkpoint in checkPoints)
         {
             BoxCollider2D checkPointCollider =
                 checkpoint.GetComponent<BoxCollider2D>();
@@ -48,10 +81,20 @@ public class GameController : MonoBehaviour
                 player.GetComponent<Player>().respawnpoint =
                     player.transform.position;
             }
-        }
+        }*/
+        
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             Pause();
+        }
+
+        if(Input.GetKeyDown(KeyCode.G)){
+           SaveManager.SaveData(player.GetComponent<Player>(), this);
+
+        }
+
+        if(Input.GetKeyDown(KeyCode.L)){
+          LoadData();
         }
     }
 
@@ -66,4 +109,22 @@ public class GameController : MonoBehaviour
             Time.timeScale = 1;
         }
     }
+
+    public void LoadData(){
+        SaveData loadedData = SaveManager.LoadData();
+        if(loadedData!=null){
+            saveData=loadedData;
+            //Debug.Log(level);
+            //Debug.Log(loadedData.level);
+            if(level!=loadedData.level){
+                SceneManager.LoadScene("Level"+loadedData.level);
+
+            }else{
+                level = saveData.level;
+                player.transform.position=new Vector3(saveData.position[0],saveData.position[1],saveData.position[2]);
+                saveData = null;
+            }
+        }
+    }
+
 }
